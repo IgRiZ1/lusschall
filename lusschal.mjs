d650adf93aca079993ecb3e346b97252f7794cbd
IgRiZ1 <dalil.blh@outlook.be>
2024-09-30 11:51:00 +0200

    Create lusschal.mjs

---
+++ lusschal.mjs
@@ -0,0 +1,11 @@
+let i = 5;
+while(i <= 5){
+    let j = 1 ;
+    while(j <= 10){
+        let result = i * j;
+        console.log(`${i} x ${j} = ${result}`);
+        j++;
+    }
+    console.log("-----------------");
+    i++;
+}
