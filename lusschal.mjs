d252f79c69630bb61f68cd6f2ada4f9221c65e7e
IgRiZ1 <dalil.blh@outlook.be>
2024-09-30 11:56:28 +0200

    Update lusschal.mjs

--- lusschal.mjs
+++ lusschal.mjs
@@ -1,11 +1,1 @@
-let i = 5;
-while(i <= 5){
-    let j = 1 ;
-    while(j <= 10){
-        let result = i * j;
-        console.log(`${i} x ${j} = ${result}`);
-        j++;
     }
-    console.log("-----------------");
-    i++;
-}
